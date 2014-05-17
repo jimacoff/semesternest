a1ce555f280b0bc90f898d22122213f83ba8d6b2
Thereza <therezascherrer@gmail.com>
2014-05-17 15:28:56 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Semesternest::Application.config.secret_key_base = 'e21a6cf473e75aaa06029ab85971e9af36f43acd776e96016ca4a4d0db84958f396ad01e4ebe589420661af5ecc8560ab2d58fadd86e5dba0fdf03cd4bcf270d'
